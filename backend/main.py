c0036d3593f3bca50b5e4bd2826f9461347f354b
Mario Sieg <mario.sieg.64@gmail.com>
2022-12-21 11:21:24 +0100

    dispatch table impl

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from url_registry import UrlRegistry
+from url_dispatch_table import UrlDispatchTable
 
 
 def main():
     print('(c) Sybit GmbH 2022')
     print('Launching...')
     registry = UrlRegistry()
+    dispatch_table = UrlDispatchTable(registry)
+    dispatch_table()
     return
 
 
--- backend/url_dispatch_table.py
+++ backend/url_dispatch_table.py
@@ -2,25 +2,62 @@ from url_registry import UrlRegistry
 
 
 class DispatchFlags:
-    NONE = 1 << 0
+    NONE = 0 << 0
     SKIP = 1 << 0
 
 
 class UrlDispatchTable(object):
+    DISPATCH_TABLE = [
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x,
+        lambda x: x
+    ]
+
     def __init__(self, registry: UrlRegistry, init_flags=DispatchFlags.NONE):
         self.table = {}
-        for url in registry.get_urls():
-            self.table[url] = (init_flags, url)
+        assert len(self.DISPATCH_TABLE) == len(registry.get_urls), 'Dispatch table length is different from url registry length'
+        for url in registry.get_urls:
+            self.table[url] = (init_flags, None)
         self._populate()
+        i: int = 0
         for k, v in self.table.items():
             assert k is not None and v is not None
             (flags, func) = v
             if (flags & DispatchFlags.SKIP) != 0:
                 continue
-            assert callable(func) and v.__name__ == '<lambda>'
+            i += 1
+            assert func is not None and callable(func) and func.__name__ == '<lambda>'
+        print(f'Dispatch table online - {i} dispatch pairs available')
+
+    def __call__(self, *args, **kwargs):
+        print(f'Invoking dispatch table... {len(self.table)} entries')
+        for k, v in self.table.items():
+            (flags, func) = v
+            func(k)
 
     def _populate(self):
-        pass
+        for i in range(len(self.DISPATCH_TABLE)):
+            self._lookup(i, self.DISPATCH_TABLE[i])
+
+    def _lookup(self, n, f):
+        assert n >= 0 and (n & ~0xFF_FF_FF_FF) == 0
+        if n < 0:
+            n += len(self.table)
+        for i, key in enumerate(self.table):
+            if i == n:
+                ll = list(self.table[key])
+                ll[1] = f
+                self.table[key] = tuple(ll)
+                return
+        raise IndexError("dictionary index out of range")
 
     def __index__(self, k):
         return self.table[k]
