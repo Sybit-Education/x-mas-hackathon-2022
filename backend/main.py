34e4ca7929873ebcf204e319eb9307c97f63fa14
Mario Sieg <mario.sieg.64@gmail.com>
2022-12-21 10:38:27 +0100

    url loading

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
+from url_registry import UrlRegistry
+
+
 def main():
     print('(c) Sybit GmbH 2022')
     print('Launching...')
+    registry = UrlRegistry()
     return
 
 
--- backend/url_registry.py
+++ backend/url_registry.py
@@ -1,13 +1,17 @@
 import json
-import os
+from os import getcwd, name as osname
+from os.path import exists
 
 
-class UrlRegister(object):
-    URL_FILE = 'url_registry.json'
+class UrlRegistry(object):
+    SEPARATOR = '\\' if osname == 'nt' else '/'
+    URL_FILE = f'{getcwd()}{SEPARATOR}url_registry.json'
 
     def __init__(self, *opt_urls):
         try:
-            self.urls = json.loads(self.URL_FILE) if os.path.exists(self.URL_FILE) else []
+            print(f'Loading urls from: {self.URL_FILE}')
+            with open(self.URL_FILE) as f:
+                self.urls = json.loads(f.read()) if exists(self.URL_FILE) else []
         except Exception as e:
             print(f'Failed to load urls from JSON: {self.URL_FILE}')
             self.urls = []
@@ -15,7 +19,10 @@ class UrlRegister(object):
         finally:
             for url in opt_urls:
                 self.urls.append(url)
-            print(f'URL registry online {self.urls} available')
+            self.urls = [(lambda x: x.lower())(x) for x in self.urls]
+            for i in range(len(self.urls)):
+                print(f'Url #{i}: \'{self.urls[i]}\'')
+            print(f'URL registry online {len(self.urls)} available')
 
     def lookup(self) -> int:
         i: int = 0
