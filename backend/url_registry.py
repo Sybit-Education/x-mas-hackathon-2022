e780cf7968b317d31fa93bec615a69ac97996679
Mario Sieg <mario.sieg.64@gmail.com>
2022-12-21 10:19:34 +0100

    Update url_registry.py

--- backend/url_registry.py
+++ backend/url_registry.py
@@ -1,6 +1,21 @@
+import json
+import os
+
+
 class UrlRegister(object):
-    def __init__(self):
-        self.urls = []
+    URL_FILE = 'url_registry.json'
+
+    def __init__(self, *opt_urls):
+        try:
+            self.urls = json.loads(self.URL_FILE) if os.path.exists(self.URL_FILE) else []
+        except Exception as e:
+            print(f'Failed to load urls from JSON: {self.URL_FILE}')
+            self.urls = []
+            raise e
+        finally:
+            for url in opt_urls:
+                self.urls.append(url)
+            print(f'URL registry online {self.urls} available')
 
     def lookup(self) -> int:
         i: int = 0
