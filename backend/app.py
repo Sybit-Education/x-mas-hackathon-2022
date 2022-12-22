9c989600cc602310f500385a8dc42ff29821e388
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-12-22 10:19:06 +0100

    chore: add miossing route

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,25 @@
 import json
-
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
+
+from services.lunch import get_today_lunch
 from services.restaurant import get_all_restaurants
 from url_registry import UrlRegistry
 from url_dispatch_table import UrlDispatchTable
 
 app = Flask(__name__)
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
 registry = UrlRegistry()
 dispatch_table = UrlDispatchTable(registry)
 dispatch_table()
+
+
+@scheduler.scheduled_job('interval', minutes=1)
+def scheduled_job():
+    print("This job is run every minute.")
 
 
 @app.route('/lunch', methods=['GET'])
@@ -16,6 +27,11 @@ def route_get_lunch():
     return json.dumps(dispatch_table(), default=vars)
 
 
-@app.route('/restaurant', methods=['GET'])
+@app.route('/v1/lunch', methods=['GET'])
+def route_today_lunch():
+    return json.dumps(get_today_lunch(), default=vars)
+
+
+@app.route('/v1/restaurant', methods=['GET'])
 def route_get_restaurant():
     return json.dumps(get_all_restaurants(), default=vars)
