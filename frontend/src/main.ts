868741c24f123a932d97fb564c8246def23b63b0
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-12-22 15:29:26 +0100

    chore: add pinia store for restaurants

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,10 +5,14 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { useRestaurantStore } from './stores/restaurant';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const restaurantStore = useRestaurantStore()
+restaurantStore.init()
 
 app.mount('#app')
---
+++ frontend/src/services/api.ts
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+const API_PREFIX = '/v1'
+
+const api = {
+  server: null,
+
+  /**
+   * Get the base-URL of the Service
+   */
+  getServer () {
+    if (this.server) {
+      return this.server
+    }
+    if (import.meta.env.VITE_APP_API_SERVER) {
+      this.server = import.meta.env.VITE_APP_API_SERVER
+    } else {
+      this.server = window.location.origin
+      if (this.server.charAt(this.server.length - 1) === '/') {
+        this.server = this.server.substring(0, this.server.length - 1)
+      }
+    }
+    console.info(`%c⚙️ Rest-Service: ${this.server}`, 'color: #5793c9')
+    return this.server
+  },
+  getApi () {
+    return this.getServer() + API_PREFIX
+  },
+  restClient () {
+    return axios.create({
+      baseURL: this.getApi(),
+      timeout: 5000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+}
+
+export default api
