8493a770f4a05cc24ab34b0ccba740068386f97f
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-12-22 11:03:31 +0100

    chore: add lunch mwethod

--- backend/services/lunch.py
+++ backend/services/lunch.py
@@ -1,5 +1,9 @@
 import os
+from datetime import datetime
+
 from pyairtable import Table
+
+from dto.menu_dto import MenuDto
 
 
 def get_today_lunch():
@@ -8,3 +12,19 @@ def get_today_lunch():
     table = Table(api_key, base_key, "Lunch")
 
     return table.all(view="today")
+
+
+def add_lunch(restaurant_id, menu_dto):
+    print("add lunch: restaurant_id=" + restaurant_id + " menu: " + menu_dto)
+
+    api_key = os.environ['AIRTABLE_API_KEY']
+    base_key = os.environ['AIRTABLE_BASE_KEY']
+    table = Table(api_key, base_key, "Lunch")
+
+    menu = dict()
+    menu["Restaurant"] = restaurant_id
+    menu["Name"] = menu_dto.name
+    menu["Description"] = menu_dto.description
+    menu["Price"] = menu_dto.price
+
+    table.create(menu)
