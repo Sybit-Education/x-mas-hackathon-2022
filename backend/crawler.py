a7894d92251637bd6da4e84fd584e9f377b99a24
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-12-22 10:41:27 +0100

    fix: add proxy

--- backend/crawler.py
+++ backend/crawler.py
@@ -1,3 +1,6 @@
+import os
+import urllib
+
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
@@ -6,6 +9,11 @@ from urllib3.util.retry import Retry
 class Crawler:
     def crawl(url: str) -> str:
         print(f'Crawling: \'{url}\'')
+
+        proxy = urllib.request.ProxyHandler({'http': os.environ['HTTP_PROXY'], 'https': os.environ['HTTPS_PROXY']})
+        opener = urllib.request.build_opener(proxy)
+        urllib.request.install_opener(opener)
+
         session = requests.Session()
         retry = Retry(connect=3, backoff_factor=0.5)
         adapter = HTTPAdapter(max_retries=retry)
