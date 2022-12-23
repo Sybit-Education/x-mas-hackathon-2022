9ad88ca55e89faf96e681efa63acfa37b9715588
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-12-23 09:49:59 +0100

    fix: formatting config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,13 +3,22 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
-  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    'vue/require-default-prop': 0,
+    'vue/html-indent': ['error', 2],
+    'vue/singleline-html-element-content-newline': 0,
+  },
+  globals: {
+    _: true,
+  },
 }
